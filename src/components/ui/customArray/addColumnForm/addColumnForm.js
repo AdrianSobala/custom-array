d2f5a904244ea26813462c6d585d645a2eee07a2
Adrian Sobala <kontakt@webhorizon.pl>
2021-03-31 23:40:58 +0200

    feat: form for generate column value and key

---
+++ src/components/ui/customArray/addColumnForm/addColumnForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Input from "../../input/input";
+
+import "./addColumnForm.scss";
+
+const AddColumnForm = (props) => {
+    return (
+        <>
+            <form>
+                <Input />
+            </form>
+        </>
+    );
+};
+
+export default AddColumnForm;
